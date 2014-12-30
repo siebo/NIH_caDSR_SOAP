4d3e142fb035c5f726fb330eb59fb93bb799a3ad
David Siedband <david@zentraal.com>
2014-12-30 04:47:02 -0800

    First draft of web service provider and stub for tests

--- caDSR_SOAP_agent/tests.py
+++ caDSR_SOAP_agent/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.test.client import Client
+from django.test.client import RequestFactory
+from .views import DjangoSoapService
 
-# Create your tests here.
+class ServiceTests(TestCase):
+    """SOAP service tests."""
+
+    def test_GET(self):
+        service_call = DjangoSoapService()
+        self.assertEquals(
+            'water',
+            'fire',
+    )
--- caDSR_SOAP_agent/views.py
+++ caDSR_SOAP_agent/views.py
@@ -1,3 +1,38 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from pysimplesoap.server import SoapDispatcher
 
-# Create your views here.
+dispatcher = SoapDispatcher(
+    'cadsr_soap_dispatcher',
+    location = "http://localhost:8000/soap",
+    action = "http://localhost:8000/soap",
+    namespace = "http://nlm.nih.gov/sdc/form",
+    prefix="ns0",
+    ns = "urn:ihe:iti:rfd:2007")
+
+class DjangoSoapService(SoapDispatcher):
+    csrf_exempt = True
+
+    def __call__(self, request):
+        response = HttpResponse(mimetype='application/xml')
+        superclass = super(DjangoSoapService, self)
+        if request.method == 'POST':
+            response.write(superclass.dispatch(request.raw_post_data))
+        elif request.method == 'GET':
+            if not self.location:
+                self.location = request.build_absolute_uri()
+            response.write(superclass.wsdl())
+        else:
+            response.status_code = 405
+        response['Content-length'] = str(len(response.content))
+        return response
+
+    def soap(self, **kwargs):
+        def wrapper(func):
+            returns = kwargs.pop('returns', None)
+            if returns and not isinstance(returns, dict):
+                returns = {'result': returns}
+            self.register_function(func.__name__, func, args=kwargs, returns=returns)
+        return wrapper
+
