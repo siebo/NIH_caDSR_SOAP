2460d7b9410abf7d0d76c1e4fd531fd6db82ca99
David Siedband <david@zentraal.com>
2015-01-02 02:35:08 -0800

    adding URL conf to map to view

--- NIH_caDSR_SOAP/urls.py
+++ NIH_caDSR_SOAP/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from caDSR_SOAP_agent.views import DjangoSoapService
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'NIH_caDSR_SOAP.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+    url('^soap/', 'DjangoSoapService.soap', name='soap'),
 )
+
