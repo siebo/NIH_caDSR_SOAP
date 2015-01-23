b91e7a475cecbe48cb84a479671ae0d224b341c6
David Siedband <david@zentraal.com>
2015-01-23 17:16:51 +0100

    Improved SOAP dispatcher with Django 1.7 compat and CRSF exemption

--- NIH_caDSR_SOAP/urls.py
+++ NIH_caDSR_SOAP/urls.py
@@ -1,14 +1,17 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from caDSR_SOAP_agent.views import DjangoSoapService
+from caDSR_SOAP_agent import views
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'NIH_caDSR_SOAP.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
-    url('^soap/', 'DjangoSoapService.soap', name='soap'),
+    url('^soap/', 'caDSR_SOAP_agent.views.DjangoSoapService', name='DjangoSoapService'),
     
     url(r'^wsdl/', 'caDSR_SOAP_agent.views.wsdl', name='wsdl'),
+    
+    url('^soap.wsdl', views.dispatcher_handler, name='soap'),
+    
     
     url(r'^$', 'caDSR_SOAP_agent.views.demo', name='demo'),
     
--- caDSR_SOAP_agent/views.py
+++ caDSR_SOAP_agent/views.py
@@ -9,6 +9,9 @@ def demo(request):
 def wsdl(request):
     return HttpResponse('NIH caDSR WSDL')
 
+def form_as_XML(request):
+    return HttpResponse('<xml></xml>')
+
 dispatcher = SoapDispatcher(
     'cadsr_soap_dispatcher',
     location = "http://localhost:8080/soap",
@@ -17,28 +20,21 @@ dispatcher = SoapDispatcher(
     prefix="ns0",
     ns = "urn:ihe:iti:rfd:2007")
 
-class DjangoSoapService(SoapDispatcher):
-    csrf_exempt = True
+# register func
+dispatcher.register_function('soap', form_as_XML,
+    returns={'FormResult': str}, 
+    args={'formID': str})
 
-    def __call__(self, request):
-        response = HttpResponse(mimetype='application/xml')
-        superclass = super(DjangoSoapService, self)
-        if request.method == 'POST':
-            response.write(superclass.dispatch(request.raw_post_data))
-        elif request.method == 'GET':
-            if not self.location:
-                self.location = request.build_absolute_uri()
-            response.write(superclass.wsdl())
-        else:
-            response.status_code = 405
-        response['Content-length'] = str(len(response.content))
-        return response
 
-    def soap(self, **kwargs):
-        def wrapper(func):
-            returns = kwargs.pop('returns', None)
-            if returns and not isinstance(returns, dict):
-                returns = {'result': returns}
-            self.register_function(func.__name__, func, args=kwargs, returns=returns)
-        return wrapper
+#delete for csrf the POST for this view
+@csrf_exempt
+def dispatcher_handler(request):
+    if request.method == "POST":
+        response = HttpResponse(dispatcher.dispatch(request.body))
+    else:
+        response = HttpResponse(dispatcher.wsdl())
+    response['Content-length'] = str(len(response.content))
+    return response
+
+
 
