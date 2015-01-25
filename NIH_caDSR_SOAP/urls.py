9735039c29a19843b88d12e70195490b5b206451
David Siedband <david@zentraal.com>
2015-01-25 04:55:11 +0100

    move URL fragmant for SOAP service

--- NIH_caDSR_SOAP/urls.py
+++ NIH_caDSR_SOAP/urls.py
@@ -3,16 +3,14 @@ from django.contrib import admin
 from caDSR_SOAP_agent import views
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'NIH_caDSR_SOAP.views.home', name='home'),
+
     url(r'^admin/', include(admin.site.urls)),
-    url('^soap/', 'caDSR_SOAP_agent.views.DjangoSoapService', name='DjangoSoapService'),
     
     url(r'^wsdl/', 'caDSR_SOAP_agent.views.wsdl', name='wsdl'),
     
     url('^soap.wsdl', views.dispatcher_handler, name='soap'),
     
-    url(r'^form/', 'caDSR_SOAP_agent.views.form_as_XML', name='form_as_XML'),
+    url(r'^soap', 'caDSR_SOAP_agent.views.form_as_XML', name='form_as_XML'),
     
     url(r'^$', 'caDSR_SOAP_agent.views.demo', name='demo'),
     
