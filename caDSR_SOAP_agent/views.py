efccf431f5feb889af6b2307501a69fbd14bf391
David Siedband <david@zentraal.com>
2015-01-25 02:08:53 +0100

    support HTML FormIDs and response payloads

--- caDSR_SOAP_agent/views.py
+++ caDSR_SOAP_agent/views.py
@@ -2,13 +2,19 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from pysimplesoap.server import SoapDispatcher
+from pysimplesoap.simplexml import SimpleXMLElement
 from NIH_caDSR_SOAP import settings
 from forms_static import adrenal
 from forms_static import demographic
 from forms_static import FDA
 from forms_static import HERF
 from forms_static import NCI_Demographics
-from pysimplesoap.simplexml import SimpleXMLElement
+from forms_static_html import adrenal_html
+from forms_static_html import demographic_html
+from forms_static_html import FDA_html
+from forms_static_html import HERF_html
+from forms_static_html import NCI_Demographics_html
+
 
 import os
 
@@ -30,6 +36,16 @@ def form_as_XML(request):
       return SimpleXMLElement(HERF)
     elif formID == 'http://nci.nih.gov/xml/owl/cadsr/form/form_design_identifier/2674812v4.0/form_package_identifier#2674812v4.0/1':
       return SimpleXMLElement(NCI_Demographics)
+    elif formID == 'Adrenal_html':
+      return SimpleXMLElement(adrenal_html)
+    elif formID == 'Demog_html':
+      return SimpleXMLElement(demographic_html)
+    elif formID == 'http://nci.nih.gov/xml/owl/cadsr/form/form_design_identifier/4617751v1.0/form_package_identifier#4617751v1.0/1/html':
+      return SimpleXMLElement(FDA_html)
+    elif formID == 'http://nci.nih.gov/xml/owl/cadsr#form_package/99999/1.0/html':
+      return SimpleXMLElement(HERF_html)
+    elif formID == 'http://nci.nih.gov/xml/owl/cadsr/form/form_design_identifier/2674812v4.0/form_package_identifier#2674812v4.0/1/html':
+      return SimpleXMLElement(NCI_Demographics_html)
     else:
       return SimpleXMLElement('<xml>Error</xml>')
 
